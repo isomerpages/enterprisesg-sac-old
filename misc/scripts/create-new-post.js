7053e68b692ada04061fd2f5fd71a22022733630
sharonent <50573648+sharonent@users.noreply.github.com>
2019-08-05 17:14:02 +0800

    Update create-new-post.js

--- misc/scripts/create-new-post.js
+++ misc/scripts/create-new-post.js
@@ -1,1 +1,7 @@
-alert("test!")
+alert(location.href);
+if (location.href.indexOf("https://github.com/isomerpages/enterprisesg-sac") < 0)) {
+  alert("This script only works in Github isomerpages enterprisesg-sac pages");
+}
+else {
+  location.href="https://github.com/isomerpages/enterprisesg-sac/new/staging/newsroom/news-releases/_posts";
+}
