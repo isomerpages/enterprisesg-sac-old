1ddfcf190eb31107986a59d69bdfbec5ec4b9ddf
sharonent <50573648+sharonent@users.noreply.github.com>
2019-08-06 09:26:28 +0800

    Update create-new-post.js

--- misc/scripts/create-new-post.js
+++ misc/scripts/create-new-post.js
@@ -1,6 +1,1 @@
-if (location.href.indexOf("https://github.com/isomerpages/enterprisesg-sac") < 0) {
-  alert("This script only works in Github isomerpages enterprisesg-sac pages");
-}
-else {
-  location.href="https://github.com/isomerpages/enterprisesg-sac/new/staging/newsroom/news-releases/_posts";
-}
+alert("Hello World");
