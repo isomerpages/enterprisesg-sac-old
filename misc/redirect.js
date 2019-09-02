7b9a996b62db6f879b35bca717249432842188e5
sharonent <50573648+sharonent@users.noreply.github.com>
2019-09-02 16:40:13 +0800

    Update redirect.js

--- misc/redirect.js
+++ misc/redirect.js
@@ -1,4 +1,4 @@
-var currentURL = window.location;
+var currentURL = window.location.toString();
 var index = currentURL.lastIndexOf(".aspx");
 if (index > -1) {
   currentURL = currentURL.substring(0,index);
