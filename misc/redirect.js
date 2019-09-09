a464cbbdae3bea9d2bc4fcd891f37e952df09c84
sharonent <50573648+sharonent@users.noreply.github.com>
2019-09-09 16:26:53 +0800

    Update redirect.js

--- misc/redirect.js
+++ misc/redirect.js
@@ -7,7 +7,7 @@ if (index > -1) {
 
 var notificationBar = document.getElementById("notification-bar");
 if (notificationBar.length > 0) {
-  var expiry = new Date("2019-09-10 00:00:00");
+  var expiry = new Date("2019-09-09 16:30:00");
   var currentDate = new Date();
   if(expiry - currentDate < 0) {
     notificationBar.style.display = "none";
