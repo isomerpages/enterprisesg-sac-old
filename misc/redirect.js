68882e9dc030e50650bc20e47fe926c82b22003e
sharonent <50573648+sharonent@users.noreply.github.com>
2019-09-09 16:24:14 +0800

    Update redirect.js

--- misc/redirect.js
+++ misc/redirect.js
@@ -4,3 +4,13 @@ if (index > -1) {
   currentURL = currentURL.substring(0,index);
   window.location.replace(currentURL);
 }
+
+var notificationBar = document.getElementById("notification-bar");
+if (notificationBar.length > 0) {
+  var expiry = new Date("2019-09-10 00:00:00");
+  var currentDate = new Date();
+  if(expiry - currentDate < 0) {
+    notificationBar.style.display = "none";
+  }
+}
+
