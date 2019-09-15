f2fec4a474b0a1267d181f6e6c1c5389693ea500
sharonent <50573648+sharonent@users.noreply.github.com>
2019-09-16 02:15:17 +0800

    Update redirect.js

--- misc/redirect.js
+++ misc/redirect.js
@@ -9,19 +9,16 @@ var content = document.getElementById("main-content");
 if (content != null) {
   var links = content.getElementsByTagName('a');
   var href = "";
-  console.log(location.origin);
   for (var i=0; i<links.length; i++) {
-    href = links[i].getAttribute("href");
-    console.log(href);
-    if (href.indexOf("/") != 0 && href.indexOf(location.origin) < 0) {
-      console.log("open in new window");
-      if (links[i].getAttribute("target") != "_blank") {
-        links[i].addEventListener('click', function (event) {
+    links[i].addEventListener('click', function (event) {
+      href = this.getAttribute("href");
+      if (href != null && href.indexOf("/") != 0 && href.indexOf(location.origin) < 0) {
+        if (!this.hasAttribute("target")) {
           event.preventDefault();
-          window[i].open(href,"_blank");
-        });
+          window.open(href,"_blank");
+        }
       }
-    }
+    });
   }
 }
 
