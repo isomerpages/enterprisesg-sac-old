17c15720ae5fc9670f1da24c1354da18160b273e
sharonent <50573648+sharonent@users.noreply.github.com>
2019-09-16 01:56:36 +0800

    Update redirect.js

--- misc/redirect.js
+++ misc/redirect.js
@@ -5,3 +5,17 @@ if (index > -1) {
   window.location.replace(currentURL);
 }
 
+var links = document.getElementsByTagName('a');
+var href = "";
+for (var i=0; i<links.length; i++) {
+  href = links[i].getAttribute("href");
+  console.log(href);
+  if (href.indexOf(location.origin) < 0) {
+    if (links[i].getAttribute("target") != "_blank") {
+      links[i].addEventListener('click', function (event) {
+        event.preventDefault();
+        links[i].open(href,"_blank");
+      });
+    }
+  }
+}
