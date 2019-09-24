0298e9f43e396cfe88eda0a7d6e9d830b2282033
sharonent <50573648+sharonent@users.noreply.github.com>
2019-09-24 09:00:40 +0800

    Update main.js

--- misc/main.js
+++ misc/main.js
@@ -6,15 +6,12 @@ if (mainContent != null) {
     var links = content.getElementsByTagName('a');
     var href = "";
     for (var i=0; i<links.length; i++) {
-      links[i].addEventListener('click', function (event) {
-        href = this.getAttribute("href");
+      if (!links[i].hasAttribute("target")) {
+        href = links[i].getAttribute("href");
         if (href != null && href.indexOf("/") != 0 && href.indexOf(location.origin) < 0 && href.indexOf('www.sac-accreditations.gov.sg') < 0) {
-          if (!this.hasAttribute("target")) {
-            event.preventDefault();
-            window.open(href,"_blank");
-          }
+          links[i].setAttribute("target", "_blank");
         }
-      });
+      }
     }
   }
 }
