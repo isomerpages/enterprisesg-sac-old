feaa1b2cd3fcb03138c4a043d389e297ee8b5070
sharonent <50573648+sharonent@users.noreply.github.com>
2019-09-24 09:10:21 +0800

    Update main.js

--- misc/main.js
+++ misc/main.js
@@ -8,7 +8,7 @@ if (mainContent != null) {
     for (var i=0; i<links.length; i++) {
       if (!links[i].hasAttribute("target")) {
         href = links[i].getAttribute("href");
-        if (href != null && href.indexOf("/") != 0 && href.indexOf(location.origin) < 0 && href.indexOf('www.sac-accreditations.gov.sg') < 0) {
+        if (href != null && href.indexOf("/") != 0 && href.indexOf("http") > 0 && href.indexOf(location.origin) < 0 && href.indexOf('www.sac-accreditations.gov.sg') < 0) {
           links[i].setAttribute("target", "_blank");
         }
       }
