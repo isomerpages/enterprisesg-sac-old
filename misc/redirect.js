874e9e54fdeddeb1ea444395bf4e781566cbe3f4
sharonent <50573648+sharonent@users.noreply.github.com>
2019-09-24 08:49:38 +0800

    Update redirect.js

--- misc/redirect.js
+++ misc/redirect.js
@@ -1,24 +1,21 @@
-var currentURL = window.location.toString();
-var index = currentURL.lastIndexOf(".aspx");
-if (index > -1) {
-  currentURL = currentURL.substring(0,index);
-  window.location.replace(currentURL);
-}
-
-var content = document.getElementById("main-content");
-if (content != null) {
-  var links = content.getElementsByTagName('a');
-  var href = "";
-  for (var i=0; i<links.length; i++) {
-    links[i].addEventListener('click', function (event) {
-      href = this.getAttribute("href");
-      if (href != null && href.indexOf("/") != 0 && href.indexOf(location.origin) < 0) {
-        if (!this.hasAttribute("target")) {
-          event.preventDefault();
-          window.open(href,"_blank");
+var mainContent = document.getElementById("main-content");
+if (mainContent != null) {
+  var contentDivs = mainContent.getElementsByClassName('content');
+  if (contentDivs.length > 0) {
+    var content = contentDivs[0];
+    var links = content.getElementsByTagName('a');
+    var href = "";
+    for (var i=0; i<links.length; i++) {
+      links[i].addEventListener('click', function (event) {
+        href = this.getAttribute("href");
+        if (href != null && href.indexOf("/") != 0 && href.indexOf(location.origin) < 0 && href.indexOf('www.sac-accreditations.gov.sg') < 0) {
+          if (!this.hasAttribute("target")) {
+            event.preventDefault();
+            window.open(href,"_blank");
+          }
         }
-      }
-    });
+      });
+    }
   }
 }
 
